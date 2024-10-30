340a03b5b2a1644574cc559bc9891a6d8e728c30
Tilak Thummanapally <82318125+Tilak0205@users.noreply.github.com>
2024-10-30 22:14:06 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: 'https://shortenurl-eduhxlm0r-tilak0205s-projects.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
