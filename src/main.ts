88e7e1d6265cbf67af36e6ec9ea3a982f74c3707
Tilak0205 <thummanapallytilak@gmail.com>
2024-10-31 14:02:42 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'https://shortenurl-eduhxlm0r-tilak0205s-projects.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-
-  // Use global validation pipe
-  app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
