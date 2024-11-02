ce56ccf2b3ea7fbe7eba914e9aee95a9283155e7
Tilak0205 <thummanapallytilak@gmail.com>
2024-11-02 16:45:49 +0530

    prettier fixes

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -3,7 +3,7 @@ import { UrlService } from './url.service';
 import { UrlController } from './url.controller';
 
 @Module({
-    controllers: [UrlController],
-    providers: [UrlService],
+  controllers: [UrlController],
+  providers: [UrlService],
 })
 export class UrlModule {}
